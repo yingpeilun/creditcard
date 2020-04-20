9b5eef2278f02320b140b4d65a0b284b79d4c209
yingpeilun <542685589@qq.com>
2020-04-21 00:10:56 +0800

    user,sms,applycard写完  21D 0:10

--- creditcard-applycard/src/main/java/com/credit/controller/ApplyController.java
+++ creditcard-applycard/src/main/java/com/credit/controller/ApplyController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @Controller
 public class ApplyController {
---
+++ creditcard-interface/src/main/java/com/credit/api/ApplyApi.java
@@ -0,0 +1,89 @@
+package com.credit.api;
+
+import com.credit.bo.PageResult;
+import com.credit.pojo.TbAppCardStatus;
+import com.credit.pojo.TbBasicInfo;
+import com.credit.pojo.TbCompanyInfo;
+import com.credit.pojo.TbContactInfo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface ApplyApi {
+
+    /**
+     * 新增基本信息
+     * @param basicInfo
+     * @return
+     */
+    @PostMapping("insertBasic")
+    public Boolean updateBasic(TbBasicInfo basicInfo);
+
+    /**
+     * 新增公司信息
+     * @param companyInfo
+     * @return
+     */
+    @PostMapping("insertCompany")
+    public Boolean insertCompany(TbCompanyInfo companyInfo);
+
+    /**
+     * 新增联系人信息
+     * @param contactInfo
+     * @return
+     */
+    @PostMapping("insertContact")
+    public Boolean insertContact(TbContactInfo contactInfo);
+
+    /**
+     * 查询卡片状态
+     * @param uid
+     * @param cardType
+     * @return
+     */
+    @GetMapping("queryCardStatus")
+    public String queryCardStatus(
+            @RequestParam("uid") Long uid,
+            @RequestParam("cardType")String cardType);
+
+
+    /**
+     * 分页工作人员审核申请卡片
+     * @param page
+     * @param rows
+     * @param grade
+     * @return
+     */
+    @GetMapping("queryAppStatus")
+    public PageResult<TbAppCardStatus> queryAppStatus(
+            @RequestParam(value = "page",defaultValue = "1") Integer page,
+            @RequestParam(value = "rows",defaultValue = "5") Integer rows,
+            @RequestParam("grade") Integer grade);
+
+    /**
+     * 根据uid和cardtype来修改卡片目前的审批阶段
+     * @param grade
+     * @param uid
+     * @param cardType
+     * @return
+     */
+    @GetMapping("alterAppStatus")
+    public Boolean alterAppStatus(
+            @RequestParam("grade") Long grade,
+            @RequestParam("uid") Long uid,
+            @RequestParam("cardType")String cardType);
+
+    /**
+     * 根据uid和cardtype来修改卡片状态为审批不通过
+     * @param uid
+     * @param cardType
+     * @return
+     */
+    @GetMapping("alterCardStatus")
+    public Boolean alterCardStatus(
+            @RequestParam("uid") Long uid,
+            @RequestParam("cardType")String cardType);
+}
