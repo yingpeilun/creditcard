30f36a99d30efb916e4c923cb55f1d13bcca3b91
15766239448 <15766239448@163.com>
2020-05-08 07:14:24 +0800

    Merge branch 'master' of https://github.com/yingpeilun/creditcard
    # Conflicts:
    #	creditcard-customer/src/main/java/com/credit/controller/NotBillController.java

--- creditcard-bill/src/main/java/com/credit/controller/Test/BeanUtils_Test.java
+++ creditcard-bill/src/main/java/com/credit/controller/Test/BeanUtils_Test.java
@@ -1,6 +1,14 @@
 package com.credit.controller.Test;
 
+import com.credit.pojo.TbHistoryEverybill;
+import com.credit.pojo.TbHistoryNotEverybill;
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.lang.reflect.InvocationTargetException;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BeanUtils_Test {
     public static void main(String[] args) {
@@ -12,6 +20,44 @@ public class BeanUtils_Test {
         System.out.println(currentYear);
         System.out.println(currentMonth);
         System.out.println(currentDay);
+        //--------------------------------------------------
+        TbHistoryEverybill vo = new TbHistoryEverybill();
+        TbHistoryNotEverybill vo1 = new TbHistoryNotEverybill();
+        vo1.setId(1L);
+        vo1.setBillDate(new Date());
+        vo1.setBillDateNum(20200508L);
+        vo1.setCcId(1111111111111111L);
+        vo1.setGetMoneyCard(2222222222222222L);
+        vo1.setMoneyType("人民币");
+        vo1.setPayAmount(1000L);
+        vo1.setPayInfo("bibiji");
+        vo1.setPayDate(new Date());
+        vo1.setPayDateNum(20200508L);
+        try {
+            BeanUtils.copyProperties(vo,vo1);
+            System.out.println(vo.getId());
+            System.out.println(vo.getMoneyType());
+            System.out.println(vo.getCcId());
+            System.out.println(vo.getBillDate());
+            System.out.println(vo.getBillDateNum());
+            System.out.println(vo.getGetMoneyCard());
+            System.out.println(vo.getPayAmount());
+            System.out.println(vo.getPayDate());
+            System.out.println(vo.getPayInfo());
+            System.out.println(vo.getPayDateNum());
+            System.out.println(vo.getRepayDateNum());
+            System.out.println(vo.getRepayDate());
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        //-----------------------------------------------
+        Map<String, Object> map = new HashMap<>();
+        map.put("d",123);
+        map.put("d",456);
+        map.put("d",789);
+        System.out.println(map.get("d"));
 
     }
 }
