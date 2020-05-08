fe21158ed1321fe6b6776476fc23d449ff69fb76
Guan-Lee <2453167787@qq.com>
2020-05-08 20:18:32 +0800

    InstalmentService更新

--- creditcard-bill/src/main/java/com/credit/listener/InstalmentNotBillListener.java
+++ creditcard-bill/src/main/java/com/credit/listener/InstalmentNotBillListener.java
@@ -16,6 +16,14 @@ public class InstalmentNotBillListener {
     @Autowired
     private NotBillService notBillService;
 
+    //还款的
+//    msg.put("bankName",bankName);//银行名
+//        msg.put("cuName",cuName);//户头名
+//        msg.put("repayMoney",repayMoney.toString());//还款金额
+//        msg.put("ccId",ccId.toString());//信用卡卡号
+//        msg.put("scId",scId.toString());//储蓄卡卡号
+//    this.rabbitTemplate.convertAndSend
+//            ("creditCard.repay.exchange","repay.money.msg",msg);
     //监听分期账单
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "creditCard.instalmentBill.queue",durable = "true"),
@@ -25,4 +33,22 @@ public class InstalmentNotBillListener {
     public void ListenInstalmentNotBill(Map<String,String> msg){
 
     }
+
+    //分期的
+//    msg.put("ccid",ccid.toString());//卡号
+//            msg.put("payInfo","分期");//交易描述
+//            msg.put("moneyType","人民币");//币种
+//    Long c=Long.valueOf(everyMoney)*num;
+//            msg.put("payAmount",c.toString());//交易总金额(分期总本金)
+//            msg.put("payDateNum",sdf.format(d));//交易日期（纯数字）
+//            msg.put("getMoneyCard","中信银行");//收款账号
+//            msg.put("billDateNum",dateformat1);//账单日(纯数字)
+//    // ==>
+//            msg.put("InstaTotal",String.valueOf(num));//分期总期数
+//            msg.put("CurrPricipal",everyMoney);//每月本金
+//            msg.put("CurrInserest",instalMoney);//每月利息
+//
+//            this.rabbitTemplate.convertAndSend("creditCard.bill.exchange","instalment.bill.msg",msg);
+//            this.rabbitTemplate.convertAndSend("creditCard.manage.exchange","instalment.money.msg",msg);
+//            return true;
 }
